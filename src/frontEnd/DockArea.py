77eb53111c38e5d6949f89444017eb8421f7c5c7
Sangavi GR <21pw28@psgtech.ac.in>
2023-06-24 14:53:18 +0530

    Update DockArea.py

--- src/frontEnd/DockArea.py
+++ src/frontEnd/DockArea.py
@@ -182,10 +182,10 @@ class DockArea(QtWidgets.QMainWindow):
         
         upload_button = QPushButton("Upload Pspice schematics")
         upload_button.clicked.connect(lambda: self.upload_file(file_path_text_box.text()))
-        self.eConLayout.addWidget(upload_button, 1, 1)
+        self.eConLayout.addWidget(upload_button, 1, 1, 1, 1)
         
         convert_button = QPushButton("Convert Pspice to eSim")
-        self.eConLayout.addWidget(convert_button, 1, 2)
+        self.eConLayout.addWidget(convert_button, 1, 2, 1, 2)
 
         self.eConWidget.setLayout(self.eConLayout)
         dock[dockName + str(count)] = QtWidgets.QDockWidget(dockName + str(count))
