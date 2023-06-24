61584f5790bbabcf20afe7b2d0b9d2c19e14b5a2
Sangavi GR <21pw28@psgtech.ac.in>
2023-06-24 13:43:58 +0530

    Update DockArea.py

--- src/frontEnd/DockArea.py
+++ src/frontEnd/DockArea.py
@@ -167,7 +167,7 @@ class DockArea(QtWidgets.QMainWindow):
         """This function creates a widget for different subcircuit options."""
         global count
 
-        dockName = 'Pspice to eSim Convertor'
+        dockName = 'Pspice to eSim Convertor-'
 
         self.subcktWidget = QtWidgets.QWidget()
         self.subcktLayout = QtWidgets.QVBoxLayout()
