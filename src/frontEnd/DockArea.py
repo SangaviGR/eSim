30bae7785946607d3cef13c8865d66a7204fcd9c
Sangavi GR <21pw28@psgtech.ac.in>
2023-06-26 17:19:05 +0530

    Update DockArea.py

--- src/frontEnd/DockArea.py
+++ src/frontEnd/DockArea.py
@@ -186,10 +186,14 @@ class DockArea(QtWidgets.QMainWindow):
         upload_button.setFixedSize(170, 30) 
         upload_button.clicked.connect(lambda: self.upload_file(file_path_text_box.text()))
         self.eConLayout.addWidget(upload_button, 1, 0, 1, 1)
+
+        self.eConLayout.addSpacing(20)
         
         convert_button = QPushButton("Convert Pspice to eSim")
         convert_button.setFixedSize(180, 30) 
         self.eConLayout.addWidget(convert_button, 1, 1, 1, 1)
+
+        self.eConLayout.addSpacing(20)
 
         convert_button1 = QPushButton("Convert LTspice to eSim")
         convert_button1.setFixedSize(180, 30) 
