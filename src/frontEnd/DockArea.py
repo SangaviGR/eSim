4269ec2266d5546e44d4d28c8b8d887285cb9c37
Sangavi GR <21pw28@psgtech.ac.in>
2023-06-27 11:56:10 +0530

    Update DockArea.py

--- src/frontEnd/DockArea.py
+++ src/frontEnd/DockArea.py
@@ -224,8 +224,8 @@ class DockArea(QtWidgets.QMainWindow):
 
     def browse_path(self, text_box):
         file_dialog = QFileDialog()  # a dialog that allows the user to select files or directories
-        file_dialog.setFileMode(QFileDialog.ExistingFile)  # Set the file mode to select existing files
-        file_dialog.setNameFilter("Schematic Files (*.sch)")  # Set the file filter
+        file_dialog.setFileMode(QFileDialog.ExistingFile)
+        file_dialog.setNameFilter("Schematic Files (*.sch)")
         file_dialog.exec_()  # Execute the dialog
         selected_files = file_dialog.selectedFiles()  # Get the selected file(s)
         if selected_files:
