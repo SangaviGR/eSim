11bae87a84014f859c6c1f708a0fc3e493a58bf9
Sangavi GR <21pw28@psgtech.ac.in>
2023-07-04 05:00:16 +0530

    Update DockArea.py

--- src/frontEnd/DockArea.py
+++ src/frontEnd/DockArea.py
@@ -249,7 +249,7 @@ class DockArea(QtWidgets.QMainWindow):
                 newFile = str(conPath + "/" + filename)
                 print(newFile)
                 self.app = Application()
-                self.app.obj_Mainview.obj_projectExplorer.addTreeNode(filename, [conPath])
+                self.app.obj_Mainview.obj_projectExplorer.addTreeNode(filename, [filename])
                 print("File added under the project explorer.")
             else:
                 # User chose not to add the file
