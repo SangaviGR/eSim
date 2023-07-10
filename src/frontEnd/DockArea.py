30f4577071128a23f51480ea32d8b5da72e93983
Sangavi GR <21pw28@psgtech.ac.in>
2023-07-11 01:44:54 +0530

    Update DockArea.py

--- src/frontEnd/DockArea.py
+++ src/frontEnd/DockArea.py
@@ -226,38 +226,43 @@ class DockArea(QtWidgets.QMainWindow):
         count = count + 1
 
     def convert_Pspice(self, file_path):
-
         # Get the base name of the file without the extension
         filename = os.path.splitext(os.path.basename(file_path))[0]
         conPath = os.path.dirname(file_path)
-        command = f"cd /home/ubuntus/eSim/schematic_converters/lib/PythonLib && python3 parser.py {file_path} {conPath}/{filename}"
+        
+        # Check if the file is not empty
+        if os.path.getsize(file_path) > 0:
+            command = f"cd /home/ubuntus/eSim/schematic_converters/lib/PythonLib && python3 parser.py {file_path} {conPath}/{filename}"
+            
+            try:
+                subprocess.run(command, shell=True, check=True)
+                # Show a message box with the conversion success message
+                msg_box = QMessageBox()
+                msg_box.setIcon(QMessageBox.Information)
+                msg_box.setWindowTitle("Conversion Successful")
+                msg_box.setText("The file has been converted successfully. Do you want to include it under the project explorer?")
+                msg_box.setStandardButtons(QMessageBox.Yes | QMessageBox.No)
+                msg_box.setDefaultButton(QMessageBox.Yes)
+                result = msg_box.exec_()
+                print("Conversion of Pspice to eSim schematic Successful")
 
-        try:
-            subprocess.run(command, shell=True, check=True)
-            # Show a message box with the conversion success message
-            msg_box = QMessageBox()
-            msg_box.setIcon(QMessageBox.Information)
-            msg_box.setWindowTitle("Conversion Successful")
-            msg_box.setText("The file has been converted successfully. Do you want to include it under the project explorer")
-            msg_box.setStandardButtons(QMessageBox.Yes | QMessageBox.No)
-            msg_box.setDefaultButton(QMessageBox.Yes)
-            result = msg_box.exec_()
-            print("Conversion of Pspice to eSim schematic Successful")
+                if result == QMessageBox.Yes:
+                    # Add the converted file under the project explorer
+                    newFile = str(conPath + "/" + filename)
+                    print(newFile)
+                    self.app = Application()
+                    self.app.obj_Mainview.obj_projectExplorer.addTreeNode(newFile, [newFile])
+                    shutil.copytree(newFile, f"/home/ubuntus/eSim-Workspace/{filename}") 
+                    print("File added under the project explorer.")
+                else:
+                    # User chose not to add the file
+                    print("File not added under the project explorer.")
+            except subprocess.CalledProcessError as e:
+                # Handle any errors that occurred during command execution
+                print("Error:", e)
+        else:
+            print("File is empty. Cannot perform conversion.")
 
-            if result == QMessageBox.Yes:
-                # Add the converted file under the project explorer
-                newFile = str(conPath + "/" + filename)
-                print(newFile)
-                self.app = Application()
-                self.app.obj_Mainview.obj_projectExplorer.addTreeNode(newFile, [newFile])
-                shutil.copytree(newFile, f"/home/ubuntus/eSim-Workspace/{filename}") 
-                print("File added under the project explorer.")
-            else:
-                # User chose not to add the file
-                print("File not added under the project explorer.")
-        except subprocess.CalledProcessError as e:
-            # Handle any errors that occurred during command execution
-            print("Error:", e)
 
 
 
