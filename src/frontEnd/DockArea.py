6f52910e378333712059e1254b23fe18f696d18a
Sangavi GR <21pw28@psgtech.ac.in>
2023-08-01 16:16:18 +0530

    Update DockArea.py

--- src/frontEnd/DockArea.py
+++ src/frontEnd/DockArea.py
@@ -224,7 +224,7 @@ class DockArea(QtWidgets.QMainWindow):
 
         self.description_label = QLabel()
         self.description_label.setFixedHeight(150)
-        self.description_label.setFixedWidth(900)
+        self.description_label.setFixedWidth(960)
 
         description_html = """
             <html>
