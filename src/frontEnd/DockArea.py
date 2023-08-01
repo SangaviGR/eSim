67aca7235ca493d4cf4725913711a758802424f0
Sangavi GR <21pw28@psgtech.ac.in>
2023-08-01 15:29:38 +0530

    Update DockArea.py

--- src/frontEnd/DockArea.py
+++ src/frontEnd/DockArea.py
@@ -223,8 +223,8 @@ class DockArea(QtWidgets.QMainWindow):
         # self.eConLayout.addWidget(text_edit)  # Add QTextEdit widget to main layout
 
         self.description_label = QLabel()
-        self.description_label.setFixedHeight(400)
-        self.description_label.setFixedWidth(900)
+        self.description_label.setFixedHeight(150)
+        self.description_label.setFixedWidth(300)
 
         description_html = """
             <html>
