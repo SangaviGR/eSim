b15acf6aaf42467c5e7434ca3f169b65f99c2c1e
Sangavi GR <21pw28@psgtech.ac.in>
2023-08-01 16:08:49 +0530

    Update DockArea.py

--- src/frontEnd/DockArea.py
+++ src/frontEnd/DockArea.py
@@ -246,7 +246,7 @@ class DockArea(QtWidgets.QMainWindow):
                             padding: 10px;
                             background-color: #165982;
                             border: 4px outset  #0E324B;
-                            min-height: 23pt;
+                            min-height: 20pt;
                         }
                     </style>
                 <head>
