94fd9f5e93937561f51c62304ebee19780d4e36b
Sangavi GR <21pw28@psgtech.ac.in>
2023-08-12 18:18:46 +0530

    Update DockArea.py

--- src/frontEnd/DockArea.py
+++ src/frontEnd/DockArea.py
@@ -280,10 +280,10 @@ class DockArea(QtWidgets.QMainWindow):
         if radio_button.isChecked():
             if radio_button.text() == "Upload Pspice schematics":
                 self.convert_button.setEnabled(True)
-                self.convert_Pspice(file_path_text_box)
+                self.convert_button.clicked.connect(lambda: self.convert_Pspice(file_path_text_box))
             elif radio_button.text() == "Upload LTspice schematics":
                 self.convert_button.setEnabled(True)
-                self.convert_Pspice(file_path_text_box)
+                self.convert_button.clicked.connect(lambda: self.convert_Pspice(file_path_text_box))
 
 
     def convert_Pspice(self, file_path):
