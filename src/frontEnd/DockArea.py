8170953fabb2f68e6b4bf27682f25ac05a6a9a88
Sangavi GR <21pw28@psgtech.ac.in>
2023-08-19 14:33:32 +0530

    Update DockArea.py

--- src/frontEnd/DockArea.py
+++ src/frontEnd/DockArea.py
@@ -324,7 +324,12 @@ class DockArea(QtWidgets.QMainWindow):
                     shutil.copytree(newFile, f"/home/ubuntus/eSim-Workspace/{filename}")
 
                     print("File added under the project explorer.")
-
+                    # Message box with the Added Successfully message
+                    msg_box = QMessageBox()
+                    msg_box.setIcon(QMessageBox.Information)
+                    msg_box.setWindowTitle("Added Successfully")
+                    msg_box.setText("File added under the project explorer successfully.")
+                    result = msg_box.exec_()
                     QtWidgets.QMainWindow.close(self)
 
                 else:
